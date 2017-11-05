1dc04310f3a083d9d073dd8e3fef5858f7e650d0
Jimmy Guzman <jimmy.guzman@outlook.com>
2017-11-04 20:24:40 -0500

    added interactive text

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+
 // You can use either a string for the player ID (i.e., `player`),
 // or `document.querySelector()` for any selector
 var player = new MediaElementPlayer('player', {
@@ -7,5 +8,21 @@ var player = new MediaElementPlayer('player', {
 // is available in the `success` callback
   success: function(mediaElement, originalNode, instance) {
 
+  }
+});
+
+const span = document.getElementById("transcript").getElementsByTagName("span");
+const video = document.getElementsByTagName("video")[0];
+
+video.addEventListener("timeupdate", () => {
+for (var i = 0; i < span.length; i++) {
+  let startTime = span[i].getAttribute("data-startTime");
+  let endTime = span[i].getAttribute("data-endTime");
+  let videoTime = video.currentTime;
+    if (videoTime >= startTime && videoTime <= endTime) {
+      span[i].classList.add("highlight");
+    } else {
+      span[i].classList.remove("highlight");
+    }
   }
 });
