101c7211886cdab10f1578e04600c3ab6619c90b
Jimmy Guzman <jimmy.guzman@outlook.com>
2017-11-05 11:42:00 -0600

    added click interaction and mouseover

--- js/app.js
+++ js/app.js
@@ -15,7 +15,7 @@ const span = document.getElementById("transcript").getElementsByTagName("span");
 const video = document.getElementsByTagName("video")[0];
 
 video.addEventListener("timeupdate", () => {
-for (var i = 0; i < span.length; i++) {
+for (let i = 0; i < span.length; i++) {
   let startTime = span[i].getAttribute("data-startTime");
   let endTime = span[i].getAttribute("data-endTime");
   let videoTime = video.currentTime;
@@ -26,3 +26,16 @@ for (var i = 0; i < span.length; i++) {
     }
   }
 });
+
+
+for (let i = 0; i < span.length; i++) {
+  span[i].addEventListener('click', () => {
+    video.currentTime = event.target.getAttribute("data-startTime");
+  });
+  span[i].addEventListener('mouseover', () => {
+    span[i].classList.add("mouseover");
+  });
+  span[i].addEventListener('mouseout', () => {
+    span[i].classList.remove("mouseover");
+  });
+}
